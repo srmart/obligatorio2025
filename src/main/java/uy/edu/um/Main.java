86c7793aa078e08638948e72868703a5281c4344
Agustina Pereyra <apereyra2@correo.um.edu.uy>
2025-06-06 14:10:19 -0300

    Otras modificaicones al menú

--- src/main/java/uy/edu/um/Main.java
+++ src/main/java/uy/edu/um/Main.java
@@ -18,59 +18,57 @@ public class Main {
             System.out.println("2. Ejecutar consultas");
             System.out.println("3. Salir");
             opcion=leer.nextInt();
+            switch (opcion){
+                case 1:
+                    //Carga de Datos
+                    break;
+                case 2:
+                    int consulta;
+                    do{
+                        System.out.println("1. Top 5 de las películas que más calificaciones por idioma.");
+                        System.out.println("2. Top 10 de las películas que mejor calificación media tienen por parte de los usuarios.");
+                        System.out.println("3. Top 5 de las colecciones que más ingresos generaron.");
+                        System.out.println("4. Top 10 de los directores que mejor calificación tienen.");
+                        System.out.println("5. Actor con más calificaciones recibidas en cada mes del año.");
+                        System.out.println("6. Usuarios con más calificaciones por género");
+                        System.out.println("7. Salir");
+                        consulta=leer.nextInt();
+                        switch (consulta){
+                            case 1:
+                                // Consulta 1
+                                break;
+                            case 2:
+                                // Consulta 2
+                                break;
+                            case 3:
+                                // Consulta 3
+                                break;
+                            case 4:
+                                // Consulta 4
+                                break;
+                            case 5:
+                                // Consulta 5
+                                break;
+                            case 6:
+                                // Consulta 6
+                                break;
+                            case 7:
+                                System.out.println("Volviendo al menú principal...");
+                                break;
+                            default:
+                                System.out.println("Opción inválida. Intente de nuevo.");
+                        }
+
+                    }while(consulta !=7);
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intente de nuevo.");
+
+
+            }
         } while(opcion != 3);
-
-        switch (opcion){
-            case 1:
-                //Carga de Datos
-                break;
-            case 2:
-                int consulta;
-                do{
-                    System.out.println("1. Top 5 de las películas que más calificaciones por idioma.");
-                    System.out.println("2. Top 10 de las películas que mejor calificación media tienen por parte de los usuarios.");
-                    System.out.println("3. Top 5 de las colecciones que más ingresos generaron.");
-                    System.out.println("4. Top 10 de los directores que mejor calificación tienen.");
-                    System.out.println("5. Actor con más calificaciones recibidas en cada mes del año.");
-                    System.out.println("6. Usuarios con más calificaciones por género");
-                    System.out.println("7. Salir");
-                    consulta=leer.nextInt();
-                    switch (consulta){
-                        case 1:
-                            // Consulta 1
-                            break;
-                        case 2:
-                            // Consulta 2
-                            break;
-                        case 3:
-                            // Consulta 3
-                            break;
-                        case 4:
-                            // Consulta 4
-                            break;
-                        case 5:
-                            // Consulta 5
-                            break;
-                        case 6:
-                            // Consulta 6
-                            break;
-                        case 7:
-                            System.out.println("Volviendo al menú principal...");
-                            break;
-                        default:
-                            System.out.println("Opción inválida. Intente de nuevo.");
-                    }
-
-                }while(consulta !=7);
-                break;
-            case 3:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Opción inválida. Intente de nuevo.");
-
-
-        }
-
     }
 }
